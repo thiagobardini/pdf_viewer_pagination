397cf26a25f98c67aac1108a9f0369343416d503
thiagobadini <thiagobardini@hotmail.com>
2021-05-08 01:33:11 -0400

    Added libraries pdf.js, Font Awesome, styling, JavaScript, HTML, CSS (needs to fix the layout - testing grid/flexbox)

---
+++ README.md
@@ -0,0 +1,10 @@
+# PDF Viewer and Pagination
+
+## Description 
+Custom PDF interface using pdf.js
+
+## Getting Started
+- This project uses libraries for PDF Reader in JavaScript [CDN - PDF JS](https://mozilla.github.io/pdf.js/), and [Font Awesome](https://fontawesome.com/start) icon library. 
+
+## Reference Documentation
+- [CDN - PDF JS](https://mozilla.github.io/pdf.js/) is heavily relies on the use of [Promises](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise). 
---
+++ js/main.js
@@ -0,0 +1,95 @@
+const url = '../docs/accent-family-product-guide.pdf'
+
+// 1. pdfDoc -> Represent the document we get from PDF JS library
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+// 2. Fetching the PDF and then put it in this canvas
+const scale = 1.2,
+  canvas = document.querySelector('#pdf-render'),
+  ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+  pageIsRendering = true;
+
+  // Get page
+  pdfDoc.getPage(num).then(page => {
+    console.log(page)
+    // Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    // Output current page
+    document.querySelector('#page-num').textContent = num;
+  });
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+// Show Prev Page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+// Show Next Page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+};
+
+// Get Document
+// Access the method from the library PDF JS using promise to refer to my globe variable
+pdfjsLib
+  .getDocument(url)
+  .promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    console.log(pdfDoc)
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
+  })
+  .catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector('.top-bar').style.display = 'none';
+  });
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
